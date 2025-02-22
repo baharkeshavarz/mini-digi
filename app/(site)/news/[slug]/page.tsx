36b31eaa62ebe095d5ec91e91cc8251f7dc5d4a0
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-02-23 00:53:39 +0330

    feat: parallel routes

--- app/(site)/news/[slug]/page.tsx
+++ app/(site)/news/[slug]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { redirect, useRouter } from "next/navigation";
 import React, { use } from "react";
 
 type NewsPageProps = {
@@ -10,11 +11,20 @@ type NewsPageProps = {
 const NewsPage: React.FC<NewsPageProps> = ({ searchParams, params }) => {
   const { slug } = use(params);
   const { lang } = use(searchParams);
+  // const router = useRouter();
+
+  const redirectMe = () => {
+    // router.push("/about");
+    redirect("/about");
+  };
 
   return (
     <div>
       <h1>News slug is: {slug} </h1>
       <h1>News lang : {lang} </h1>
+      <button type="button" className="bg-red-600 p-2" onClick={redirectMe}>
+        REDIRECT
+      </button>
     </div>
   );
 };
---
+++ app/complex-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full text-center bg-blue-100">
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default page;
